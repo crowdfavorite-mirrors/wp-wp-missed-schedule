eb8970636b2b1df1fd42b34d85fa89d7cda451b8
ivdimova <ivdimova@gmail.com>
2014-09-22 15:22:45 +0300

    Updating to version 2013.0730.7777

--- wp-missed-schedule.php
+++ wp-missed-schedule.php
@@ -1,24 +1,25 @@
 <?php 
-
 /*
-Plugin Name: WP Missed Schedule
+Plugin Name: WP Missed Schedule - RC2013
 Plugin URI: http://slangji.wordpress.com/wp-missed-schedule/
-Description: WP <code>Missed Schedule</code> Fix Future Posts: find Scheduled that match this problem, and it Republish them Correctly 10 items per session, every 1 minute. All others will be solved on next sessions, until no longer exist: 10 failed future posts every minute, 600 failed future posts every hour, 1 session every minute, 60 sessions every hour. The default 10 Failed Future Posts per session, was introduced for compatibility with default WordPress Items Feed Syndication. The configuration of this plugin is Automattic! Work under <a href="http://www.gnu.org/licenses/gpl-2.0.html" title"GPLv2 or later License compatible">GPLv2</a> or later License. <a href="http://www.gnu.org/prep/standards/standards.html" title"GNU Style indentation coding standard compatible">GNU Style</a> indentation compatible. | <a href="http://slangji.wordpress.com/donate/" title="Free Donation">Donate</a> | <a href="http://slangji.wordpress.com/contact/" title="Send Me Bug and Suggestion">Contact</a> | <a href="http://wordpress.org/plugins/wp-overview-lite/" title="Show DashBoard Overview and Footer Memory Load Usage">WP Overview?</a> | <a href="http://wordpress.org/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend User Profile and Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/plugins/wp-toolbar-removal/" title="Remove ToolBar Frontend Backend User Profile and Code">ToolBar Removal?</a> | <a href="http://wordpress.org/plugins/wp-login-deindexing/" title="Total DeIndexing WordPress LogIn from all Search Engines">LogIn DeIndexing?</a> | <a href="http://wordpress.org/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a> | <a href="http://wordpress.org/plugins/wp-ie-enhancer-and-modernizer/" title="Enhancer and Modernizer IE Surfing Expirience">Enhancer IE Surfing?</a> | <a href="http://wordpress.org/plugins/wp-wp-memory-db-indicator/" title="Memory Load Consumption db size Usage Indicator">Memory and db Indicator?</a> | <a href="http://wordpress.org/plugins/wp-header-and-footer-log/" title="Insert Informational Text Log on Header and Footer when Plugin is Activated">Header and Footer Log?</a>
-Version: 2013.0531.4444
+Description: WP Missed Schedule Fix Future Posts Virtual Cron Job: find only Scheduled that match this problem, no others or not Missed posts, and it Republish them Correctly 10 items per session, every 5 minutes. All others will be solved on next sessions, to no waste resources, until no longer exist; 10 failed future posts every 5 minutes, 120 failed future posts every hour, 1 session every 5 minutes, 12 sessions every hour. The default 10 Failed Future Posts per session, was introduced for compatibility with default WordPress Items Feed Syndication. This plugin is designed, on fact, for heavy use of Scheduled Future Posts and RSS Grabbing (as FeedWordPress or WP-O-Matic), but also work well with a simple WordPress Blog or for use as a CMS. The configuration of this plugin is automatic and not need other actions except installing, uninstall or delete it! Compatible with HyperDB Table Query Formatting. Release Candidate 2013 ...
+Version: 2013.0730.7777
 Author: sLa NGjI's
 Author URI: http://slangji.wordpress.com/
 Requires at least: 2.6
-Tested up to: 3.6
+Tested up to: 3.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Indentation: GNU style coding standard
 Indentation URI: http://www.gnu.org/prep/standards/standards.html
  *
- * DEVELOPMENT Release: Version 2013 Build 0530-BUGFIX.1840-DEVELOPMENTAL
+ * DEVELOPMENT release: Version 2013 Build 0824 Revision 0312
  *
- * [WP Missed Schedule](http://wordpress.org/plugins/wp-missed-schedule/) Fix Missed Scheduled Future Posts Cron Job
+ * PRO release: Version 2014 Build 0000 Revision 2015
  *
- * Copyright (C) 2008-2013 [sLa NGjI's](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ * [WP Missed Schedule](http://wordpress.org/plugins/wp-missed-schedule/) Fix Missed Scheduled Future Posts Virtual Cron Job
+ *
+ * Copyright (C) 2008-2014 [slangjis](http://slangji.wordpress.com/) (email: <slangjis [at] googlegmail [dot] com>))
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the [GNU General Public License](http://wordpress.org/about/gpl/)
@@ -35,7 +36,7 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * √ DISCLAIMER
+ * DISCLAIMER
  *
  * The license under which the WordPress software is released is the GPLv2 (or later) from the
  * Free Software Foundation. A copy of the license is included with every copy of WordPress.
@@ -50,93 +51,112 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  *
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ THERMS
+ * THERMS
  *
  * This uses (or it parts) code derived from
  *
- * wp-header-footer-log.php by sLa <slangji[at]gmail[dot]com>
- * Copyright (C) 2008-2013 [sLa NGjI's](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ * wp-header-footer-log.php by slangjis <slangjis [at] googlemail [dot] com>
+ * Copyright (C) 2008-2013 [slangjis](http://slangji.wordpress.com/) (email: <slangjis [at] googlemail [dot] com>)
  *
  * according to the terms of the GNU General Public License version 2 (or later)
  *
  * This wp-header-footer-log.php uses (or it parts) code derived from
  *
- * wp-footer-log.php by sLa <slangji[at]gmail[dot]com>
- * Copyright (C) 2008-2013 [sLa NGjI's](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ * wp-footer-log.php by slangjis <slangjis [at] googlemail [dot] com>
+ * Copyright (C) 2008-2013 [slangjis](http://slangji.wordpress.com/) (email: <slangjis [at] googlemail [dot] com>)
  *
- * sLa2sLaNGjIs.php by sLa <slangji[at]gmail[dot]com>
- * Copyright (C) 2009-2013 [sLa NGjI's](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ * sLa2sLaNGjIs.php by slangjis <slangjis [at] googlemail [dot] com>
+ * Copyright (C) 2009-2013 [slangjis](http://slangji.wordpress.com/) (email: <slangjis [at] googlemail [dot] com>)
  *
  * according to the terms of the GNU General Public License version 2 (or later)
  *
  * According to the Terms of the GNU General Public License version 2 (or later) part of Copyright belongs to your own author and part belongs to their respective others authors:
  *
- * Copyright (C) 2008-2013 [sLa NGjI's](http://slangji.wordpress.com/) (email: <slangji[at]gmail[dot]com>)
+ * Copyright (C) 2008-2013 [slangjis](http://slangji.wordpress.com/) (email: <slangjis [at] googlemail [dot] com>)
  *
- * √ VIOLATIONS
+ * VIOLATIONS
  *
  * [Violations of the GNU Licenses](http://www.gnu.org/licenses/gpl-violation.en.html)
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ GUIDELINES
+ * GUIDELINES
  *
  * This software meet [Detailed Plugin Guidelines](http://wordpress.org/plugins/about/guidelines/) paragraphs 1,4,10,12,13,16,17 quality requirements.
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ CODING
+ * CODING
  *
  * This software implement [GNU style](http://www.gnu.org/prep/standards/standards.html) coding standard indentation.
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
  *
- * √ VALIDATION
+ * VALIDATION
  *
  * This readme.txt rocks. Seriously. Flying colors. It meet the specifications according to WordPress [Readme Validator](http://wordpress.org/plugins/about/validator/) directives.
  * The author of this plugin is available at any time, to make all changes, or corrections, to respect these specifications.
+ *
+ * THANKS
+ *
+ * [nicokaiser](http://wordpress.org/support/topic/plugin-uses-post_date_gmt-which-is-not-indexed)
+ * Jack Hayhurst <jhayhurst [at] liquidweb [dot] com> for MySQL Queries and Server Load Optimization and Index Suggestion ...
+ * [Arkadiusz Rzadkowolski](http://profiles.wordpress.org/fliespl/) for resolving table_name from query broken in select query: HyperDB Compatibility!
+ * [milewis1](http://profiles.wordpress.org/milewis1/) for WordPress blog's timezone implementation instead of the MySQL time.
  */
 
-/**
- * @package WP Missed Schedule
- * @subpackage WordPress PlugIn
- * @description Fix Missed Scheduled Future Posts Cron Job
- * @since 2.6.0
- * @version 2013.0531.4444
- * @status STABLE (release)
- * @author sLa NGjI's
- * @license GPLv2 or later
- * @indentation GNU style coding standard
- * @keybit b79H8651411574J4YQCeLCQM540kT29FPANa8zFXj3lC62m78BbFMtb3g46FsK338
- * @keysum CD55116071EBB7A748520CD47560D020
- * @keytag edcb48f0d130ca1974cf9bab6cb27fc3
- */
+	/**
+	 * @package WP Missed Schedule
+	 * @subpackage WordPress PlugIn
+	 * @description Fix Missed Scheduled Future Posts Virtual Cron Job
+	 * @since 2.6.0
+	 * @tested 3.8.0
+	 * @version 2013.0730.7777
+	 * @1stversion 2008.1210.2008
+	 * @status STABLE release
+	 * @install The configuration of this Plugin is automatic!
+	 * @author sLa NGjI's
+	 * @license GPLv2 or later
+	 * @indentation GNU style coding standard
+	 * @keybit b79H8651411574J4YQCeLCQM540kT29FPANa8zFXj3lC62m78BbFMtb3g46FsK338
+	 * @keysum DB2472371629E91C58E80D8051805BC7
+	 * @keytag d76ca983e4e0b1146b8511f40cd66015
+	 */
 
-if ( !function_exists( 'add_action' ) )
-	{
-		header( 'HTTP/0.9 403 Forbidden' );
-		header( 'HTTP/1.0 403 Forbidden' );
-		header( 'HTTP/1.1 403 Forbidden' );
-		header( 'Status: 403 Forbidden' );
-		header( 'Connection: Close' );
-			exit();
-	}
+	if ( !function_exists( 'add_action' ) )
+		{
+			header( 'HTTP/0.9 403 Forbidden' );
+			header( 'HTTP/1.0 403 Forbidden' );
+			header( 'HTTP/1.1 403 Forbidden' );
+			header( 'Status: 403 Forbidden' );
+			header( 'Connection: Close' );
+				exit();
+		}
 
-define( 'WPMS_OPTION', 'wp_missed_schedule' );
+	global $wp_version;
+
+	if ( $wp_version < 2.6 )
+		{
+			wp_die( __( 'This Plugin Requires WordPress 2.6+ or Greater: Activation Stopped!' ) );
+		}
+
+	define( 'WPMS_OPTION', 'wp_missed_schedule' );
 
 	function wpms_init()
 		{
 			$last = get_option( WPMS_OPTION, false );
 
-			if ( ( $last !== false ) && ( $last > ( time() - ( 1 * 60 ) ) ) )
+			if ( ( $last !== false ) && ( $last > ( time() - ( 5 * 60 ) ) ) )
 				return;
 
 			update_option( WPMS_OPTION, time() );
 
 			global $wpdb;
 
-			$sql = $wpdb->prepare(
-			"SELECT`ID`FROM`{$wpdb->posts}`"."WHERE("."((`post_date`>0)&&(`post_date`<=%s))OR"."((`post_date_gmt`>0)&&(`post_date_gmt`<=%s))".")AND`post_status`='future'LIMIT 10",
-			current_time( 'mysql' ),
-			current_time( 'mysql', 1 )
-			);
+$qry = <<<SQL
+
+			SELECT ID FROM {$wpdb->posts} WHERE ( ( post_date > 0 && post_date <= %s ) ) AND post_status = 'future' LIMIT 0,10
+
+SQL;
+
+			$sql = $wpdb->prepare( $qry, current_time( 'mysql', 0 ) );
 
 			$scheduledIDs=$wpdb->get_col($sql);
 
@@ -149,21 +169,33 @@ define( 'WPMS_OPTION', 'wp_missed_schedule' );
 						continue;
 
 					wp_publish_post( $scheduledID );
+
 				}
 		}
 	add_action( 'init', 'wpms_init', 0 );
 
-function wpms_log()
-	{
-		echo "\n<!--Plugin WP Missed Schedule 2013.0531.4444 Active - Tag: ".md5(md5("b79H8651411574J4YQCeLCQM540kT29FPANa8zFXj3lC62m78BbFMtb3g46FsK338"."CD55116071EBB7A748520CD47560D020"))."-->\n\n";
-	}
-add_action( 'wp_head', 'wpms_log' );
-add_action( 'wp_footer', 'wpms_log' );
+	function wpms_rml( $links, $file )
+		{
+			if ( $file == plugin_basename( __FILE__ ) )
+				{
+					$links[] = '<a href="http://slangji.wordpress.com/donate/">' . __( 'Donate', 'wpms' ) . '</a>';
+					$links[] = '<a href="http://slangji.wordpress.com/contact/">' . __( 'Contact', 'wpms' ) . '</a>';
+					$links[] = '<a href="http://slangji.wordpress.com/plugins/">' . __( 'Others plugins', 'wpms' ) . '</a>';
+				}
+			return $links;
+		}
+	add_filter( 'plugin_row_meta', 'wpms_rml', 10, 2 );
 
-function wpms_db_clnp()
-	{
-		delete_option( WPMS_OPTION );
-	}
-register_deactivation_hook( __FILE__, 'wpms_db_clnp', 0 );
+	function wpms_log()
+		{
+			echo "\n<!--Plugin WP Missed Schedule 2013.0730.7777 Active - Tag ".md5(md5("b79H8651411574J4YQCeLCQM540kT29FPANa8zFXj3lC62m78BbFMtb3g46FsK338"."DB2472371629E91C58E80D8051805BC7"))."-->\n\n";
+		}
+	add_action( 'wp_head', 'wpms_log' );
+	add_action( 'wp_footer', 'wpms_log' );
 
+	function wpms_clnp()
+		{
+			delete_option( WPMS_OPTION );
+		}
+	register_deactivation_hook( __FILE__, 'wpms_clnp', 0 );
 ?>
